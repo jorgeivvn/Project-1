0114865d7d0d27b7ab863183d4f7909d368c81f0
Jorge Rodriguez <j_rod15@msn.com>
2017-12-26 23:17:54 -0700

    3rd commit- Added cards in HTML and CSS

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-//pseudo-code: 1. Display a grid of 4x4 block of cubes. 2. Click on a cube and display it's color. 3. Click on another cube display it's color 3. Determine if both colors are a match, if match let player know, else flip cubes back to original state. 
+//pseudo-code: 1. Display a grid of 4x4 block of cubes. 2. Click on a cube and display it's color. 3. Click on another cube display it's color 4. Determine if both colors are a match, if match let player know and disappear, else flip cubes back to original state. 5. Let player know he won once all cubes have disappeared.
+
+let cubeArray = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9'];
+
+let cubeValues = [];
